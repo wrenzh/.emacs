75d2f1a205a0c3de0e74d6c30b1d68e0b55f6408
Wen Zhang <wren.zh@gmail.com>
2019-09-05 11:12:05 -0400

    Fix tracking issue

--- custom.el
+++ custom.el
@@ -7,14 +7,9 @@
    (quote
     ("bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
  '(lsp-pyls-plugins-pylint-enabled nil)
- '(package-quickstart t)
  '(package-selected-packages
-<<<<<<< HEAD
-   '(ivy-bibtex flyspell-correct-ivy company-quickhelp ivy-postframe matlab evil-surround company-math company-lsp lsp-mode doom-modeline ebib all-the-icons yasnippet-snippets yasnippet leetcode diminish pyvenv evil-mc evil-magit magit counsel ivy evil-leader evil neotree all-the-icons-dired key-chord pdf-tools monokai-theme auctex company use-package)))
-=======
    (quote
-    (ivy-posframe posframe ivy-bibtex flyspell-correct-ivy company-quickhelp ivy-postframe matlab matlab-mode evil-surround company-math company-lsp lsp-mode doom-modeline ebib all-the-icons yasnippet-snippets yasnippet company-auctex leetcode company-jedi diminish pyvenv evil-mc evil-magit magit counsel ivy evil-leader evil neotree all-the-icons-dired key-chord pdf-tools monokai-theme auctex company use-package))))
->>>>>>> Add ivy-posframe
+    (ivy-posframe ivy-bibtex flyspell-correct-ivy company-quickhelp ivy-postframe matlab evil-surround company-math company-lsp lsp-mode doom-modeline ebib all-the-icons yasnippet-snippets yasnippet leetcode diminish pyvenv evil-mc evil-magit magit counsel ivy evil-leader evil neotree all-the-icons-dired key-chord pdf-tools monokai-theme auctex company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
