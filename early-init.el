04909aa14bfb1b1cb37b8b4f57762f533f4ab7fe
Wren Zhang <wren.zh@gmail.com>
2021-01-06 15:08:47 -0500

    All all-the-icons for ivy-rich

--- early-init.el
+++ early-init.el
@@ -6,10 +6,8 @@
 		    :family "Fantasque Sans Mono"
 		    :height (if (eq window-system 'ns) 140 120))
 (menu-bar-mode 0)
-(when window-system
-  (scroll-bar-mode 0)
-  (tooltip-mode 0)
-  (blink-cursor-mode 0)
-  (tool-bar-mode 0)
-  (add-to-list 'default-frame-alist (cons 'width 200))
-  (add-to-list 'default-frame-alist (cons 'height 60)))
+(scroll-bar-mode 0)
+(tooltip-mode 0)
+(tool-bar-mode 0)
+(add-to-list 'default-frame-alist (cons 'width 120))
+(add-to-list 'default-frame-alist (cons 'height 40))
--- init.el
+++ init.el
@@ -110,7 +110,7 @@
   :config (global-auto-revert-mode t))
 
 (use-package gruvbox-theme
-  :hook (after-init . (lambda () (load-theme 'gruvbox-dark-hard t))))
+  :hook (after-init . (lambda () (load-theme 'gruvbox t))))
 
 (use-package diminish)
 
@@ -145,56 +145,16 @@
 (use-package ivy-rich
   :after ivy-posframe
   :config
-  (setq ivy-rich-display-transformers-list
-	'(ivy-switch-buffer
-	  (:columns
-	   ((ivy-switch-buffer-transformer (:width 40))
-	    (ivy-rich-switch-buffer-size (:width 7))
-	    (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
-	    (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
-	    (ivy-rich-switch-buffer-project (:width 15 :face success))
-	    (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path
-							      x (ivy-rich-minibuffer-width 0.3))))))
-	   :predicate
-	   (lambda (cand) (get-buffer cand)))
-	  counsel-find-file
-	  (:columns
-	   ((ivy-read-file-transformer (:width 40))
-	    (ivy-rich-counsel-find-file-truename (:face font-lock-doc-face))))
-	  counsel-M-x
-	  (:columns
-	   ((counsel-M-x-transformer (:width 40))
-	    (ivy-rich-counsel-function-docstring (:width 40 :face font-lock-doc-face))))
-	  counsel-describe-function
-	  (:columns
-	   ((counsel-describe-function-transformer (:width 40))
-	    (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
-	  counsel-describe-variable
-	  (:columns
-	   ((counsel-describe-variable-transformer (:width 40))
-	    (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))
-	  counsel-recentf
-	  (:columns
-	   ((ivy-rich-candidate (:width 40))
-	    (ivy-rich-file-last-modified-time (:face font-lock-comment-face))))
-	  package-install
-	  (:columns
-	   ((ivy-rich-candidate (:width 40))
-	    (ivy-rich-package-version (:width 16 :face font-lock-comment-face))
-	    (ivy-rich-package-archive-summary (:width 7 :face font-lock-builtin-face))
-	    (ivy-rich-package-install-summary (:face font-lock-doc-face))))))
-  (ivy-rich-mode 0)
   (ivy-rich-mode t))
 
 (use-package ivy-posframe
-  :if window-system
   :diminish ivy-posframe-mode
   :after ivy
   :config
-  (setq ivy-posframe-height 10)
+  (setq ivy-posframe-height 12)
   (setq ivy-posframe-width 80)
   (setq ivy-posframe-display-functions-alist
-	'((t . ivy-posframe-display-at-frame-center)))
+	'((t . ivy-posframe-display-at-frame-top-center)))
   (setq ivy-posframe-parameters
 	'((left-fringe . 10)
 	  (right-fringe . 10)))
@@ -228,6 +188,7 @@
     "b" 'ivy-switch-buffer
     "d" 'dired
     "e" 'find-file
+    "f" 'counsel-rg
     "C" 'kill-buffer-and-window
     "c" 'kill-this-buffer
     "w" 'save-buffer
@@ -258,7 +219,7 @@
   :defer 1
   :config
   (setq company-idle-delay 0.1)
-  (setq company-minimum-prefix-length 1)
+  (setq company-minimum-prefix-length 2)
   :hook
   (emacs-lisp-mode . (lambda () (company-mode t))))
 
@@ -332,16 +293,26 @@
 (use-package eglot
   :hook
   (python-mode . eglot-ensure))
-(put 'dired-find-alternate-file 'disabled nil)
 
 (use-package doom-modeline
-  :config (setq doom-modeline-icon nil)
+  :config
+  (setq doom-modeline-icon t)
+  (setq doom-modeline-height 30)
   :hook (after-init . doom-modeline-mode))
 
 (use-package org
   :config
-  (setq org-agenda-files (list "~/OneDrive/Documents/Notes/Schedule.org")))
+  (setq org-agenda-files (list "~/OneDrive/Documents/Notes/Schedule.org"))
+  :hook (org-mode . olivetti-mode))
 
 (use-package vterm
   :config
+  (setq vterm-always-compile-module t)
   (evil-leader/set-key "v" 'vterm))
+
+(use-package all-the-icons
+  :config (setq all-the-icons-scale-factor 1.1))
+
+(use-package all-the-icons-ivy-rich
+  :after ivy-rich
+  :config (all-the-icons-ivy-rich-mode))
